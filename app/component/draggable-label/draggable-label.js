4fc0927cf8ed0b9c29602888ebda28b828949ec2
Uladzimir Shein <uladzimir_shein@epam.com>
2015-06-12 21:07:11 +0300

    Created correct draggable groups

--- app/component/draggable-label/draggable-label.js
+++ app/component/draggable-label/draggable-label.js
@@ -1,96 +1,106 @@
-angular.module('component.draggable-label').directive('draggableLabel', function (deltaDragHandler) { //jshint ignore: line
-        var SNAP_SENSITIVITY = 0.2;
+angular.module('component.draggable-label').directive('draggableLabel', function ($timeout) { //jshint ignore: line
+    var SNAP_SENSITIVITY = 0.2;
 
-        function snapValue(val, sensivity) {
-            var ending = Math.abs(val % 1);
-            if (1 - sensivity >= ending && ending >= sensivity) {
-                return val;
-            } else {
-                return Math.round(val);
-            }
+    function snapValue(val, sensivity) {
+        var ending = Math.abs(val % 1);
+        if (1 - sensivity >= ending && ending >= sensivity) {
+            return val;
+        } else {
+            return Math.round(val);
         }
+    }
 
-        return {
-            replace: true,
-            scope: {
-                group: '=',
-                height: '=labelHeight',
-                width: '='
-            },
-            require: ['^draggableLabelsControl', '^scrollableElement'],
-            link: function ($scope, $element, ignored, require) {
-                var draggableLabelsControl = require[0],
-                    scrollableElement = require[1],
-                    groupWidth = $scope.width,
-                    groupHeight = $scope.height;
+    return {
+        replace: true,
+        scope: {
+            group: '=',
+            height: '=labelHeight',
+            width: '='
+        },
+        require: ['^draggableLabelsControl', '^scrollableElement'],
+        link: function ($scope, $element, ignored, require) {
+            var draggableLabelsControl = require[0];
+            var scrollableElement = require[1];
+            var groupWidth = $scope.width;
 
-                function setTop(top) {
-                    $element.css({top: (top * groupHeight + 4) + 'px'});
-                }
+            function setLeft(leftPx) {
+                $element.css({'margin-left': leftPx * groupWidth  + 'px'});
+            }
 
-                function setLeft(leftPx) {
-                    $element.css({left: (leftPx * groupWidth + 4) + 'px'});
-                }
+            function setWidth(width) {
+                $element.css('width', width * groupWidth  + 'px');
+            }
 
-                function setWidth(width) {
-                    $element.css('width', (width * groupWidth - 4 * 2) + 'px');
-                }
+            $scope.$watch('group.$lineSpace', setLeft);
+            $scope.$watch('group.width', setWidth);
 
-                $scope.$watch('group.start', setLeft);
-                $scope.$watch('group.index', setTop);
-                $scope.$watch('group.width', setWidth);
+            var group = $scope.group;
 
-                var $deltaDraggableElement = deltaDragHandler($element);
+            if (group.$lastTouched) {
+                delete group.$lastTouched;
+                $timeout(function(){
+                    $element.addClass('blink');
+                    $timeout(function(){
+                        $element.removeClass('blink');
+                    }, 500, false)
+                }, 100, false);
+            }
+            var initialWidth, initialLeft, clone;
 
-                var initialWidth, initialTop, initialLeft;
-                $deltaDraggableElement.start(function () {
-                    $scope.$apply(function(){
-                        $scope.group.highlightTasks(true);
+            $scope.dragHandler = {
+                start: function () {
+                    clone = $element.clone().css({opacity: 0.5,'margin-top': '28px'});
+                    $element.after(clone).addClass('draggy');
+                    clone.parent().prepend($element);
+
+                    $scope.$apply(function () {
+                        group.highlightTasks(true);
                     });
-                    scrollableElement.watchMouse();
-                    initialLeft = $scope.group.start;
-                    initialTop = $scope.group.index;
-                    initialWidth = $scope.group.width;
-                });
 
-                function updatePosition(deltaX, deltaY, sensivity) {
-                    var snapX = snapValue(deltaX / groupWidth, sensivity);
+                    scrollableElement.watchMouse();
+                    initialLeft = group.start;
+                    initialWidth = group.width;
+                    setLeft(group.start);
+                },
+                move: function (deltaX) {
+                    var snapX = snapValue(deltaX / groupWidth, SNAP_SENSITIVITY);
                     if (!$scope.resize) {
                         if (initialLeft + snapX >= 0) {
-                            $scope.group.start = initialLeft + snapX;
+                            group.start = initialLeft + snapX;
                         }
-                        draggableLabelsControl.updateIndex($scope.group.index, initialTop + Math.round(deltaY / groupHeight));
                     } else if (initialWidth - snapX >= 1 && initialLeft + snapX >= 0) {
-                        $scope.group.start = initialLeft + snapX;
-                        $scope.group.width = initialWidth - snapX;
+                        group.start = initialLeft + snapX;
+                        group.width = initialWidth - snapX;
                     }
-                    $scope.$digest();
-                }
 
-                $deltaDraggableElement.move(function (deltaX, deltaY) {
-                    updatePosition(deltaX, deltaY, SNAP_SENSITIVITY);
-                });
-
-                $deltaDraggableElement.stop(function (deltaX/*, deltaY*/) {
-                    $scope.group.highlightTasks(false);
+                    setLeft(group.start);
+                    setWidth(group.width);
+                },
+                stop:function (deltaX) {
+                    clone.after($element.removeClass('draggy')).remove();
+                    group.highlightTasks(false);
                     scrollableElement.stopWatching();
                     var snapX = snapValue(deltaX / groupWidth, 1);
                     if (snapX) {
-                        if ($scope.group.width === initialWidth) {
-                            $scope.group.start = initialLeft + snapX;
-                            $scope.group.shrink(snapX);
-                        } else if ($scope.group.width !== initialWidth) {
-                            $scope.group.width = initialWidth - snapX;
-                            $scope.group.start = initialLeft + snapX;
-                            $scope.group.expand();
+                        group.start = initialLeft + snapX;
+                        if (group.width === initialWidth) {
+                            group.shrink(snapX);
+                        } else {
+                            group.width = initialWidth - snapX;
+                            group.expand();
                         }
+                        group.$lastTouched = true;
+                        draggableLabelsControl.recalculatePositions();
                     } else {
-                        $scope.group.width = initialWidth;
-                        $scope.group.start = initialLeft;
+                        group.width = initialWidth;
+                        group.start = initialLeft;
+                         setLeft(group.$lineSpace)
                     }
+
                     $scope.$apply();
-                });
-            },
-            templateUrl: 'app/component/draggable-label/draggable-label.html'
-        };
-    });
+                }
+            };
+        },
+        templateUrl: 'app/component/draggable-label/draggable-label.html'
+    };
+});
--- app/component/draggable-labels-control/draggable-labels-control.js
+++ app/component/draggable-labels-control/draggable-labels-control.js
@@ -1,31 +1,46 @@
 angular.module('component.draggable-labels-control', []).directive('draggableLabelsControl', function () {
+    function add(line, group) {
+        group.$lineSpace = group.start - line.width;
+        line.width = group.$width;
+        delete group.$width;
+
+        line.groups.push(group);
+    }
+
     return {
         scope: {groups: '=', columns: '='},
         replace: true,
         templateUrl: 'app/component/draggable-labels-control/draggable-labels-control.html',
-        controller: function ($scope, $element, $attrs) {
-            $scope.$watch('groups.length', function (length) {
-                var height = length ? length * $attrs.labelHeight + 4 : 0;
-                $element.css('height', height + 'px');
-            });
+        controller: function ($scope) {
 
-            this.updateIndex = function (formIndex, toIndex) {
-                if (toIndex === formIndex || toIndex < 0 || toIndex > $scope.groups.length - 1) {
-                    return;
+            this.recalculatePositions = function() {
+                var lines = [];
+
+                function createLine(){
+                    var line = {width: 0, groups: []};
+                    lines.push(line);
+                    return line;
                 }
+                var maxVal = $scope.columns.length - 1;
 
-                var from = $scope.groups.filter(function (item) {
-                    return item.index === formIndex;
-                })[0];
-
-                var to = $scope.groups.filter(function (item) {
-                    return item.index === toIndex;
-                })[0];
-                from.index = toIndex;
-                to.index = formIndex;
-
-                $scope.$digest();
+                $scope.groups.map(function(group){return group}).sort(function(a, b){
+                    return a.start > b.start ? 1 : -1;
+                }).forEach(function(group){
+                    var hasNoLine = true;
+                    group.$width = group.start + group.width;
+                    lines.forEach(function(line){
+                        if (hasNoLine && line.width <= group.start && line.width + group.$width <= maxVal) {
+                            add(line, group);
+                            hasNoLine = false;
+                        }
+                    });
+                    if (hasNoLine) {
+                        add(createLine(), group);
+                    }
+                });
+                $scope.lines = lines;
             };
+            this.recalculatePositions()
         }
     };
 });
