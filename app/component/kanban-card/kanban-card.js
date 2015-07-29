bc5810f35585e325b54a6f1163c2dca1bf7a11c3
Stanislau Tsiuryn <psyq@me.com>
2015-07-29 20:07:27 +0300

    DE5506: owenrAppLogo is not working + removed fast click for tablets

--- app/component/kanban-card/kanban-card.js
+++ app/component/kanban-card/kanban-card.js
@@ -2,9 +2,22 @@ angular.module('component.kanban-card').directive('kanbanCard', function (extend
     return {
         templateUrl: 'app/component/kanban-card/kanban-card.html',
         link: function (scope) {
-            scope.clickCallbacks = function(task, settings){
+            scope.clickCallbacks = function (task, settings, $event) {
                 extendedCard.open(task, settings);
             };
         }
     };
-});
+}).directive('ngLazyClick', ['$parse', function($parse) {
+    return {
+        compile: function($element, attr) {
+            var fn = $parse(attr["ngLazyClick"]);
+            return function handler(scope, element) {
+                element.on('click', function(event) {
+                    scope.$apply(function() {
+                        fn(scope, {$event:event});
+                    });
+                });
+            };
+        }
+    };
+}]);
