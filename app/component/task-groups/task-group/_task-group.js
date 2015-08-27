55ef8b1cfc26f158fa16b65860fe508fe88ada1c
vlad <vlad@cog1.com>
2015-08-27 18:02:16 +0200

    DE6272 [Kanban] Details pop-up window is not fully displayed

--- app/component/task-groups/task-group/_task-group.js
+++ app/component/task-groups/task-group/_task-group.js
@@ -2,5 +2,5 @@ angular.module('component.task-groups.task-group', [
     'component.glyph-icon',
     'component.task-groups.task-group-list',
     'component.opacity-on-changed',
-    'component.task-groups.task-group-modal'
+    'component.modal.task-group-modal'
 ]);
