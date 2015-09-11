d587b86a55c78022a629729ce625ecb4efcb76e5
Stanislau Tsiuryn <stanislau@cog1.com>
2015-09-11 17:52:49 +0300

    bind-html for card&&card-modal task's name

---
+++ app/component/sanitize-filter/sanitize-filter.js
@@ -0,0 +1,6 @@
+angular.module('component.sanitize-filter', [])
+    .filter("sanitize", function ($sce) {
+        return function (htmlCode) {
+            return $sce.trustAsHtml(htmlCode);
+        }
+    });
