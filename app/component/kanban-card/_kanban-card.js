8c6ff507cd631f282e7f8217231f218f817d0c89
Stanislau Tsiuryn <shumsky@me.com>
2015-10-28 20:36:02 +0300

    TA55937 : Implement confirmation modal to add friction

--- app/component/kanban-card/_kanban-card.js
+++ app/component/kanban-card/_kanban-card.js
@@ -1,6 +1,7 @@
 angular.module('component.kanban-card', [
     'component.glyph-icon',
     'component.modals.task-card-modal',
+    'component.modals.confirmation-modal',
     'component.is-touch',
     'component.tool-tip',
     'ngSanitize'
