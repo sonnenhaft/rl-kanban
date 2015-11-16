7928b04fa46aa66056e3d9e3245b58888641844a
vlad <vlad@cog1.com>
2015-11-16 23:15:10 +0100

    added sub repo properly

--- gulp/tasks/svn/cleanup-svn-checkouts.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var util = require('gulp-util');
-var clean = require('gulp-clean');
-
-gulp.task('cleanup-svn-checkouts', function () {
-    util.log('deleting ./svn-temp');
-    return gulp.src('svn-temp', {read: false})
-        .pipe(clean({force: true}));
-});
