565a43b3586812cfb7ac92bdaad2774ffd8fb190
vlad <vlad@cog1.com>
2015-11-21 12:30:25 +0100

    Updated reges to calculate coverage - excluded some more demo code.

--- app/demo-code/kanban-demo.js
+++ app/demo-code/kanban-demo.js
@@ -2,6 +2,7 @@ angular.module('kanban-demo', [
     'demo-code.events-debugger',
     'demo-code.kanban-version' ,
     'demo-code.layout-switcher',
+    'demo-code.groups-debug',
     'kanban'
 ]).config(function(kanbanVersionValueProvider, kanbanVersion){
     kanbanVersionValueProvider.set(kanbanVersion);
