e41ae809bd59a2c9010cfe9ccfc160d2a3b0c88c
vlad <vlad@cog1.com>
2015-11-21 03:53:05 +0100

    Updated some test configs.

--- app/component/isTouch/test/is-touch.spec.js
+++ app/component/isTouch/test/is-touch.spec.js
@@ -1,46 +1,36 @@
-describe('component.is-touch-1', function () {
+describe('component.is-touch', function () {
     var provider;
+    describe('with emulated window', function () {
+        beforeEach(module('component.is-touch', function (isTouchProvider, $provide) {
+            provider = isTouchProvider;
+            $provide.value('$window', {
+                document: angular.noop,
+                DocumentTouch: jasmine.createSpy('DocumentTouch')
+            });
+        }));
 
-    beforeEach(module('component.is-touch', function(isTouchProvider) {
-        provider = isTouchProvider;
-    }));
+        it('tests the providers internal function', inject(function ($window) {
+            expect(provider.$get($window)).toBe(false);
+        }));
+    });
 
-    it('tests the providers internal function', inject(function($window) {
-        expect(provider.$get($window)).toBe(true);
-    }));
-});
+    describe('without  emulated window', function () {
+        beforeEach(module('component.is-touch', function (isTouchProvider) {
+            provider = isTouchProvider;
+        }));
 
-describe('component.is-touch-false', function() {
-    var provider;
+        it('tests the providers internal function', inject(function ($window) {
+            expect(provider.$get($window)).toBe(true);
+        }));
+    });
 
-    beforeEach(module('component.is-touch-2', function(isTouchProvider) {
-        provider = isTouchProvider;
-    }));
+    describe('not touch', function () {
+        beforeEach(module('component.is-touch', function ($provide) {
+            $provide.value('isTouch', false);
+        }));
 
-    beforeEach(module(function($provide) {
-        $provide.value('$window', {
-            document: angular.noop,
-            DocumentTouch: jasmine.createSpy('DocumentTouch')
-        });
-    }));
-
-    it('tests the providers internal function', inject(function($window) {
-        expect(provider.$get($window)).toBe(false);
-    }));
-});
-
-describe('isNotTouch', function () {
-    var isNotTouch;
-    beforeEach(module('component.is-touch'));
-    beforeEach(module(function($provide){
-        $provide.value('isTouch', false);
-    }));
-
-    beforeEach(inject(function(_isNotTouch_){
-        isNotTouch = _isNotTouch_;
-    }));
-
-    it('tests the providers internal function', function() {
-        expect(isNotTouch).toBe(true)
+        it('tests the providers internal function', inject(function (isNotTouch) {
+            expect(isNotTouch).toBe(true)
+        }));
     });
 });
