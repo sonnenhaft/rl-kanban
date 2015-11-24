dab7320cab0fe89a873da5058261c123ee28cea0
Vladimir Shein <ushein@edmunds.com>
2015-11-24 23:07:06 +0100

    Updated night watch test.

---
+++ tests/nightwatch/drag-and-drop.spec.js
@@ -0,0 +1,31 @@
+module.exports = {
+    'checking cards drag and drop': function (browser) {
+        browser
+            .url('http://localhost:4000')
+            .waitForElementVisible('kanban-card', 10000)
+            .assert.elementPresent('swim-lane:nth-child(2)  kanban-column:nth-child(1)  kanban-card')
+            .moveToElement('kanban-card', 150, 150)
+            .mouseButtonDown(0)
+            .moveToElement('kanban-column:nth-child(1)', 20, 250)
+            .moveToElement('kanban-column:nth-child(3)', 20, 250)
+            .moveToElement('kanban-column:nth-child(4)', 20, 250)
+            .mouseButtonUp(0)
+            .assert.elementNotPresent('swim-lane:nth-child(2)  kanban-column:nth-child(1)  kanban-card')
+            .end();
+    },
+    'card should not be dropped into dissalowed column': function (browser) {
+        browser
+            .url('http://localhost:4000')
+            .waitForElementVisible('kanban-card', 10000)
+            .assert.elementPresent('swim-lane:nth-child(2)  kanban-column:nth-child(1)  kanban-card')
+            .moveToElement('kanban-card', 10, 10)
+            .mouseButtonDown(0)
+            .moveToElement('kanban-column:nth-child(1)', 20, 250)
+            .moveToElement('kanban-column:nth-child(3)', 20, 250)
+            .moveToElement('kanban-column:nth-child(4)', 20, 250)
+            .moveToElement('kanban-column:nth-child(5)', 20, 250)
+            .mouseButtonUp(0)
+            .assert.elementPresent('swim-lane:nth-child(2)  kanban-column:nth-child(1)  kanban-card')
+            .end();
+    }
+};
