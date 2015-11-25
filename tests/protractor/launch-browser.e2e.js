6f62928ce1984b22a4802f7cca8f6d9bd9dfbeef
Vladimir Shein <ushein@edmunds.com>
2015-11-25 23:19:07 +0100

    Updated nightwatch

---
+++ tests/protractor/launch-browser.e2e.js
@@ -0,0 +1,6 @@
+describe('Filters box', function() {
+    it('should open filters dropdown', function() {
+        browser.get('http://localhost:4000/#/stub');
+        browser.sleep(10000);
+    });
+});
