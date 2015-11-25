2c5f98450cc45a106abcd4bc35350a75193cf260
Stanislau Tsiuryn <stanislau@cog1.com>
2015-11-25 21:28:09 +0300

    fix for sticky header

--- app/component/sticky-header/sticky-header.js
+++ app/component/sticky-header/sticky-header.js
@@ -1,43 +1,39 @@
-angular.module('component.stickyHeader', []).directive('stickyHeader', function ($window) {
-    var $root = angular.element($window);
+angular.module('component.stickyHeader', []).directive('stickyHeader', function ($window, $timeout) {
     return {
         link: function ($scope, $element) {
             var iElement = $element[0];
             var isStuck = false;
-            var start = iElement.getBoundingClientRect().top + $window.document.body.scrollTop;
+            var start = iElement.getBoundingClientRect().top + ($window.document.body.scrollTop || $window.document.documentElement.scrollTop);
             var style = $window.getComputedStyle(iElement);
             var wrapper = $element.wrap('<div></div>').parent();
+            var $root = angular.element($window);
 
             function scrollSpy() {
                 var pos = $window.document.body.scrollTop || $window.document.documentElement.scrollTop;
+                recheckPositions();
                 if (!isStuck && pos > start) {
                     wrapper.css({height: iElement.offsetHeight + parseInt(style.marginTop) + parseInt(style.marginBottom) + 'px'});
-
-                    $element.css({width: iElement.offsetWidth + 'px', position: 'fixed',top: 0});
-
+                    $element.css({width: iElement.offsetWidth + 'px', position: 'fixed', top: 0});
                     isStuck = true;
                 } else if (isStuck && pos <= start) {
                     wrapper.css({height: ''});
-
-                    $element.css({width: '',position: '',top: ''});
-
+                    $element.css({width: '', position: '', top: ''});
                     isStuck = false;
                 }
             }
 
             function recheckPositions() {
                 if (!isStuck) {
-                    start =iElement.getBoundingClientRect().top + $window.document.body.scrollTop;
+                    start = iElement.getBoundingClientRect().top + $window.document.body.scrollTop;
                 }
             }
 
-            angular.element($window.document).ready(recheckPositions);
             $root.on('scroll', scrollSpy);
             $root.on('resize', recheckPositions);
 
             $scope.$on('$destroy', function () {
                 $root.off('resize', recheckPositions);
-                $root.on('scroll', scrollSpy);
+                $root.off('scroll', scrollSpy);
                 wrapper.remove();
             });
         }
