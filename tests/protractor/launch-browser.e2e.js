681f410e5a9d78b2ce00f65458fd11854f810a78
Vladimir Shein <ushein@edmunds.com>
2015-11-26 00:25:06 +0100

    fixed path to selenium log

--- tests/protractor/launch-browser.e2e.js
+++ tests/protractor/launch-browser.e2e.js
@@ -2,5 +2,6 @@ describe('Filters box', function() {
     it('should open filters dropdown', function() {
         browser.get('http://localhost:4000/#/stub');
         browser.sleep(10000);
+        expect($('kanban-card').isDisplayed()).toEqual(true);
     });
 });
