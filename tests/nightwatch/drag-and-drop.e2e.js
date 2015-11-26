3a7e0f86608fd0532528da0dc053a1a7d0224d86
Vladimir Shein <ushein@edmunds.com>
2015-11-27 00:01:57 +0100

    updated protractor tests

--- tests/nightwatch/drag-and-drop.e2e.js
+++ tests/nightwatch/drag-and-drop.e2e.js
@@ -1,8 +1,6 @@
 module.exports = {
     'checking cards drag and drop': function (browser) {
-        browser
-            .url('http://localhost:4000')
-            .waitForElementVisible('kanban-card', 10000)
+        browser.url('http://localhost:4000').waitForElementVisible('kanban-card', 10000)
             .assert.elementPresent('swim-lane:nth-child(2)  kanban-column:nth-child(1)  kanban-card')
             .moveToElement('kanban-card', 150, 150)
             .mouseButtonDown(0)
@@ -14,9 +12,7 @@ module.exports = {
             .end();
     },
     'card should not be dropped into denied column': function (browser) {
-        browser
-            .url('http://localhost:4000')
-            .waitForElementVisible('kanban-card', 10000)
+        browser.url('http://localhost:4000').waitForElementVisible('kanban-card', 10000)
             .assert.elementPresent('swim-lane:nth-child(2)  kanban-column:nth-child(1)  kanban-card')
             .moveToElement('kanban-card', 10, 10)
             .mouseButtonDown(0)
