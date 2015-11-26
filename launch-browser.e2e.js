dcd71c19d42b172fad9e8e8628c63f699b9d9727
Vladimir Shein <ushein@edmunds.com>
2015-11-26 01:05:46 +0100

    updated logger

--- launch-browser.e2e.js
+++ launch-browser.e2e.js
@@ -1,7 +1,7 @@
 describe('Filters box', function() {
     it('should open filters dropdown', function() {
         browser.get('http://localhost:4000/#/stub');
-        browser.sleep(10000);
+        browser.waitForAngular();
         expect($('kanban-card').isDisplayed()).toEqual(true);
     });
 });
