7e5dc2c74be61901e11c5703963e08cb17abc319
Vladimir Shein <ushein@edmunds.com>
2015-12-02 01:18:36 +0100

    fixing protractor test - added collapse expand for layout switcher

--- tests/protractor/drag-and-drop.e2e.js
+++ tests/protractor/drag-and-drop.e2e.js
@@ -4,7 +4,7 @@ describe('drag-and-drop', function () {
             return browser.actions()
                 .mouseMove((columnSelector))
                 .perform()
-                .then(function () {return browser.sleep(10);})
+                .then(function () {return browser.sleep(0.5);})
         }
     }
 
@@ -13,38 +13,37 @@ describe('drag-and-drop', function () {
         return expect(firstCard.isPresent())
     }
 
-    function getTestCard(){
+    function getTestCard() {
         return element(by.cssContainingText('kanban-card', 'qui nulla et incididunt consectetur'));
     }
 
-    it('checking cards drag and drop', function () {
+    var columns, movePromise;
+    beforeEach(function () {
         browser.get('http://localhost:4000/#/stub');
         expectFirstCardPresent().toBe(true);
-        var columns = $$('swim-lane').first().$$('kanban-column');
-        browser.actions().mouseDown(getTestCard()).perform()
+        columns = $$('swim-lane').first().$$('kanban-column');
+        movePromise = browser.actions().mouseDown(getTestCard()).perform()
             .then(moveToColumn(columns.first()))
             .then(moveToColumn(columns.get(2)))
-            .then(moveToColumn(columns.get(3)))
-            .then(function () {
-                return browser.actions().mouseUp().perform();
-            });
+            .then(moveToColumn(columns.get(3)));
+    });
+
+    it('checking cards drag and drop', function () {
+        movePromise.then(function () {
+            return browser.actions().mouseUp().perform();
+        });
         expectFirstCardPresent().toBe(false);
-        expect(getTestCard().isPresent()).toBe(true);
     });
 
     it('card should not be dropped into denied column', function () {
-        browser.get('http://localhost:4000/#/stub');
+        movePromise.then(moveToColumn(columns.get(4))).then(function () {
+            return browser.actions().mouseUp().perform();
+        });
         expectFirstCardPresent().toBe(true);
-        var columns = $$('swim-lane').first().$$('kanban-column');
-        browser.actions().mouseDown($('kanban-card')).perform()
-            .then(moveToColumn(columns.first()))
-            .then(moveToColumn(columns.get(2)))
-            .then(moveToColumn(columns.get(3)))
-            .then(moveToColumn(columns.get(4)))
-            .then(function () {
-                return browser.actions().mouseUp().perform();
-            });
-        expectFirstCardPresent().toBe(true);
+    });
+
+    afterEach(function () {
         expect(getTestCard().isPresent()).toBe(true);
     });
+
 });
