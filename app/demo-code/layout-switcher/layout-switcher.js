d5468fb69cc47df216c4d603f79e282561eb887e
Vladimir Shein <ushein@edmunds.com>
2015-12-02 01:18:36 +0100

    fixing protractor test - added collapse expand for layout switcher

--- app/demo-code/layout-switcher/layout-switcher.js
+++ app/demo-code/layout-switcher/layout-switcher.js
@@ -8,7 +8,7 @@ angular.module('demo-code.layout-switcher').directive('layoutSwitcher', function
             $scope.hash.contentLevel = $scope.hash.contentLevel || 'maximum';
             ['layout', 'contentLevel', 'readOnly', 'empty'].forEach(function (key) {
                 $scope.$watch('hash.' + key, function (value) {
-                    $location.search(key, value);
+                    $location.search(key, value || null);
                 });
             });
 
