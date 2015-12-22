8872f534c8de43f0dc07929ae3a57b79dc2b05a7
Vladimir Shein <ushein@edmunds.com>
2015-12-22 21:08:44 +0100

    DE8068 Work Tracker - It's impossible to delet card

--- app/component/modals/confirmation-modal/confirmation-modal.js
+++ app/component/modals/confirmation-modal/confirmation-modal.js
@@ -4,22 +4,20 @@ angular.module('component.modals.confirmation-modal', [
     'template/modal/backdrop.html',
     'template/modal/window.html'
 ]).factory('openConfirmationModal', function ($modal) {
-    return {
-        open: function ($scope) {
-            var modal = $modal.open({
-                windowClass: 'tiny confirmation-modal',
-                templateUrl: 'app/component/modals/confirmation-modal/confirmation-modal.html',
-            });
+    return function ($scope) {
+        var modal = $modal.open({
+            windowClass: 'tiny confirmation-modal',
+            templateUrl: 'app/component/modals/confirmation-modal/confirmation-modal.html',
+        });
 
-            if (!$scope) {
-                $scope.$on('$destroy', function () {
-                    if (modal) { modal.dismiss('cancel'); }
-                });
-            }
-
-            return modal.result.finally(function () {
-                modal = null;
+        if ($scope) {
+            $scope.$on('$destroy', function () {
+                if (modal) { modal.dismiss('cancel'); }
             });
         }
+
+        return modal.result.finally(function () {
+            modal = null;
+        });
     };
 });
