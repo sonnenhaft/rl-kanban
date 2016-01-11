5685b56df32c6a05f1046f959f2de4ff511549b5
Stanislau Tsiuryn <stanislau@cog1.com>
2016-01-11 22:01:36 +0300

    kanban-board.js unit test

--- app/component/kanban-board/test/kanban-board.spec.js
+++ app/component/kanban-board/test/kanban-board.spec.js
@@ -1,25 +1,73 @@
 describe.module('component.kanban-board', function () {
-    var directive;
-    beforeEach(inject(function (directiveBuilder) {
-        directive = directiveBuilder.$build('<kanban-board swimlane="swimlane" settings="settings"></kanban-board>', {
-            swimlane: {columns: {}},
-            settings: {}
-        }, {
-            kanban: {},
-            horizontalScroll: {
-                $element: angular.element('<div>')
+    var element, scope, sourceScopeMock, destScopeMock;
+    var taskMock = {
+        group: {},
+        validStates: [],
+        column: {
+            tasks: []
+        },
+        moveToColumn: jasmine.createSpy('moveToColumn')
+    };
+
+    beforeEach(inject(function ($rootScope, $compile) {
+        element = angular.element('<kanban-board swimlane="swimlane" settings="settings"></kanban-board>');
+
+        var kanbanWrapper = element.wrap('<div></div>').parent();
+        var parentScope = $rootScope.$new();
+
+        var kanbanController = {
+            highlightTask: jasmine.createSpy('highlightTask'),
+            validateStates: jasmine.createSpy('validateStates'),
+            checkEditableSwimlanes: jasmine.createSpy('checkEditableSwimlanes'),
+            getHighlighted: jasmine.createSpy('getHighlighted').and.returnValue([taskMock]),
+            clearInvalidStates: jasmine.createSpy('clearInvalidStates'),
+            activeScrollableElement: {
+                watchMouse: jasmine.createSpy('watchMouse'),
+                stopWatching: jasmine.createSpy('stopWatching')
             }
-        });
+        };
+        var horizontalScroll = {
+            $element: angular.element('<div>')
+        };
+        var scrollableElement = {
+            watchMouse: jasmine.createSpy('watchMouse'),
+            stopWatching: jasmine.createSpy('stopWatching')
+        };
+        sourceScopeMock = {itemScope: {task: angular.copy(taskMock)}, $parent: {column: {swimlane: {}}}};
+        destScopeMock = {$parent: {column: {swimlane: {}}, $parent: {$parent: {$parent: {scrollableElement: scrollableElement}}}}};
+
+        parentScope.settings = {
+            highlightTaskOnClick: true,
+            editableSwimlanes: true
+        };
+        parentScope.swimlane = {
+            columns: {}
+        };
+        kanbanWrapper.data('$kanbanController', kanbanController);
+        kanbanWrapper.data('$horizontalScrollController', horizontalScroll);
+        kanbanWrapper.data('$scrollableElementController', scrollableElement);
+        $compile(element)(parentScope);
+        parentScope.$digest();
+        scope = element.isolateScope();
     }));
 
     //TODO: split this test stub into multiply tests
     it('should be defined', function () {
-        expect(directive.element).toBeDefined();
-        var scope = directive.element.isolateScope().scrollCallbacks;
-        scope.dragStart({source: {itemScope: {task: {}}}});
-        scope.itemMoved({
-            source: {itemScope: {task: {moveToColumn: angular.noop}}},
-            dest: {sortableScope: {$parent: {column: {swimlane: {}}}}}
+        expect(element).toBeDefined();
+        var scrollCallbacks = scope.scrollCallbacks;
+        scrollCallbacks.dragStart({source: sourceScopeMock});
+        scrollCallbacks.itemMoved({
+            source: {itemScope: sourceScopeMock},
+            dest: {sortableScope: destScopeMock}
         });
+        scrollCallbacks.orderChanged({source: sourceScopeMock});
+        scrollCallbacks.dragEnd({source: sourceScopeMock});
+        scrollCallbacks.accept(sourceScopeMock, destScopeMock);
+    });
+
+    it('should emit add resource event', function () {
+        spyOn(scope, '$emit');
+        scope.addResources();
+        expect(scope.$emit).toHaveBeenCalledWith('kanban:add-task-assessment', scope.swimlane.id);
     });
 });
