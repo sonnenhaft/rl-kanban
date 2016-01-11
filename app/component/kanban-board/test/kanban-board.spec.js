3f5ac3144dcb420231c544e81800123a76253887
Stanislau Tsiuryn <stanislau@cog1.com>
2016-01-11 22:01:36 +0300

    kanban-board.js unit test

--- app/component/kanban-board/test/kanban-board.spec.js
+++ app/component/kanban-board/test/kanban-board.spec.js
@@ -1,15 +1,19 @@
 describe.module('component.kanban-board', function () {
-    var element, scope, sourceScopeMock, destScopeMock;
-    var taskMock = {
-        group: {},
-        validStates: [],
-        column: {
-            tasks: []
-        },
-        moveToColumn: jasmine.createSpy('moveToColumn')
-    };
+    var element, scope, sourceScopeMock, destScopeMock, swimlaneMock, columnMock, taskMock, scrollCallbacks;
 
     beforeEach(inject(function ($rootScope, $compile) {
+        swimlaneMock = {};
+        columnMock = {
+            tasks: [],
+            swimlane: swimlaneMock
+        };
+        taskMock = {
+            group: {},
+            validStates: [],
+            column: columnMock,
+            moveToColumn: jasmine.createSpy('moveToColumn')
+        };
+
         element = angular.element('<kanban-board swimlane="swimlane" settings="settings"></kanban-board>');
 
         var kanbanWrapper = element.wrap('<div></div>').parent();
@@ -33,12 +37,13 @@ describe.module('component.kanban-board', function () {
             watchMouse: jasmine.createSpy('watchMouse'),
             stopWatching: jasmine.createSpy('stopWatching')
         };
-        sourceScopeMock = {itemScope: {task: angular.copy(taskMock)}, $parent: {column: {swimlane: {}}}};
-        destScopeMock = {$parent: {column: {swimlane: {}}, $parent: {$parent: {$parent: {scrollableElement: scrollableElement}}}}};
+        sourceScopeMock = {task: taskMock, sortableScope: {insertItem: jasmine.createSpy('removeItem')}, itemScope: {task: taskMock}, $parent: {column: columnMock}};
+        destScopeMock = {task: taskMock, removeItem: jasmine.createSpy('removeItem'), $parent: {column: columnMock, $parent: {$parent: {$parent: {scrollableElement: scrollableElement}}}}};
 
         parentScope.settings = {
             highlightTaskOnClick: true,
-            editableSwimlanes: true
+            editableSwimlanes: true,
+            acceptTasks: true
         };
         parentScope.swimlane = {
             columns: {}
@@ -49,20 +54,94 @@ describe.module('component.kanban-board', function () {
         $compile(element)(parentScope);
         parentScope.$digest();
         scope = element.isolateScope();
+        scrollCallbacks = scope.scrollCallbacks;
     }));
 
     //TODO: split this test stub into multiply tests
     it('should be defined', function () {
         expect(element).toBeDefined();
-        var scrollCallbacks = scope.scrollCallbacks;
+    });
+
+    it('scroll callbacks should have drag start handler', function () {
+        scrollCallbacks.dragStart({source: sourceScopeMock});
+    });
+
+    it('scroll callbacks should have item moved handler', function () {
+        scrollCallbacks.itemMoved({
+            source: {itemScope: sourceScopeMock},
+            dest: {sortableScope: destScopeMock}
+        });
+        columnMock.$barred = true;
+        swimlaneMock.isTeam = false;
+        scrollCallbacks.itemMoved({
+            source: {itemScope: sourceScopeMock},
+            dest: {sortableScope: destScopeMock}
+        });
+    });
+
+    it('scroll callbacks should have order changed handler', function () {
+        scrollCallbacks.orderChanged({source: sourceScopeMock});
+    });
+
+    it('scroll callbacks should have drag end handler', function () {
+        scrollCallbacks.dragEnd({source: sourceScopeMock});
+        taskMock.validStates = null;
+        scrollCallbacks.dragEnd({source: sourceScopeMock});
+    });
+
+    it('scroll callbacks should have drag end handler', function () {
+        scrollCallbacks.accept(sourceScopeMock, destScopeMock);
+        swimlaneMock.isTeam = true;
+        scrollCallbacks.accept(sourceScopeMock, destScopeMock);
+        swimlaneMock.isTeam = false;
+        columnMock.$collapsed = true;
+        scrollCallbacks.accept(sourceScopeMock, destScopeMock);
+        swimlaneMock.isTeam = false;
+        columnMock.$collapsed = false;
+        scrollCallbacks.accept(sourceScopeMock, destScopeMock);
+        scope.settings.acceptTasks = false;
+        scrollCallbacks.accept(sourceScopeMock, destScopeMock);
+    });
+
+    it('team swimlanes should accept cards', function () {
+        columnMock.$barred = false;
+        scope.settings.highlightTaskOnClick = false;
+        swimlaneMock.isTeam = true;
+        scrollCallbacks.itemMoved({
+            source: {itemScope: sourceScopeMock},
+            dest: {sortableScope: destScopeMock}
+        });
+    });
+
+    it('should process multiselect', function () {
+        scope.settings.highlightTaskOnClick = true;
+        sourceScopeMock.task = angular.copy(taskMock);
+        scrollCallbacks.itemMoved({
+            source: {itemScope: sourceScopeMock},
+            dest: {sortableScope: destScopeMock}
+        });
+    });
+
+    it('should handle drag start event', function () {
+        taskMock.group = angular.noop();
+        scope.settings.highlightTaskOnClick = false;
+        taskMock.validStates = null;
+        scope.settings.editableSwimlanes = false;
+        scrollCallbacks.dragStart({source: sourceScopeMock});
+    });
+
+    it('should rollback scrolls', function () {
+        columnMock.$barred = true;
+        swimlaneMock.isTeam = false;
+        scrollCallbacks.itemMoved({
+            source: {itemScope: sourceScopeMock},
+            dest: {sortableScope: destScopeMock}
+        });
         scrollCallbacks.dragStart({source: sourceScopeMock});
         scrollCallbacks.itemMoved({
             source: {itemScope: sourceScopeMock},
             dest: {sortableScope: destScopeMock}
         });
-        scrollCallbacks.orderChanged({source: sourceScopeMock});
-        scrollCallbacks.dragEnd({source: sourceScopeMock});
-        scrollCallbacks.accept(sourceScopeMock, destScopeMock);
     });
 
     it('should emit add resource event', function () {
