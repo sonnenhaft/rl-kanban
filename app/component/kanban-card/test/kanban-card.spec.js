4dc40d62126c20ad8c2d8409eb2a779e80a010d6
Stanislau Tsiuryn <stanislau@cog1.com>
2016-01-12 23:10:38 +0300

    kanban-card.js unit tests

--- app/component/kanban-card/test/kanban-card.spec.js
+++ app/component/kanban-card/test/kanban-card.spec.js
@@ -1,11 +1,13 @@
 describe.module('component.kanban-card', function () {
-    var directive;
+    var directive, kanbanMock, settingsMock;
     beforeEach(inject(function (directiveBuilder) {
+        kanbanMock = {highlightTask: jasmine.createSpy('highlightTask')};
+        settingsMock = {notes: true, highlightTaskOnClick: true, enableMultiSelect: true};
         directive = directiveBuilder.$build('<kanban-card as-sortable-item></kanban-card>', {
             task: {column: {swimlane: {$disabled: false}}},
-            settings: {notes: true, highlightTaskOnClick: false}
+            settings: settingsMock
         }, {
-            kanban: {highlightTask: angular.noop},
+            kanban: kanbanMock,
             asSortable: {scope: {modelValue: {}}}
         });
     }));
@@ -31,4 +33,72 @@ describe.module('component.kanban-card', function () {
         directive.scope.settings.highlightTaskOnClick = true;
         directive.element.child('.card-wrapper').triggerHandler('click');
     }));
+
+    it('should highlight task', function () {
+        var task = {$edit: false};
+        directive.scope.highlightTask(task, {});
+        expect(kanbanMock.highlightTask).toHaveBeenCalled();
+    });
+
+    it('should not highlight task in edit mode', function () {
+        var task = {$edit: true};
+        directive.scope.highlightTask(task, {});
+        expect(kanbanMock.highlightTask).not.toHaveBeenCalled();
+    });
+
+    it('should toggle highlight task if ctrl or command key is pressed', function () {
+        var task = {$highlight: true};
+        directive.scope.highlightTask(task, {metaKey: true});
+        expect(kanbanMock.highlightTask).not.toHaveBeenCalled();
+        expect(task.$highlight).toBe(false);
+        directive.scope.highlightTask(task, {ctrlKey: true});
+        expect(task.$highlight).toBe(true);
+    });
+
+    it('should unhighlight tasks if multiselect is turned off', function () {
+        var task = {$highlight: true};
+        settingsMock.enableMultiSelect = false;
+        directive.scope.highlightTask(task, {});
+        expect(kanbanMock.highlightTask).toHaveBeenCalled();
+    });
+
+    it('should not toggle highlight task if multiselect is turned off', function () {
+        var task = {$highlight: true};
+        settingsMock.enableMultiSelect = true;
+        directive.scope.highlightTask(task, {});
+        expect(kanbanMock.highlightTask).not.toHaveBeenCalled();
+    });
+
+    it('should toggle full description', function () {
+        var task = {$highlight: false, notes: 'test'};
+        directive.scope.showFullDescription(task, {}, {
+            stopPropagation: angular.noop
+        });
+        expect(directive.scope.limit).toEqual(4)
+    });
+
+    it('should not toggle full description if task in edit mode', function () {
+        var task = {$highlight: false, notes: 'test', $edit: true};
+        directive.scope.showFullDescription(task, {}, {
+            stopPropagation: angular.noop
+        });
+        expect(directive.scope.limit).toEqual(50)
+    });
+
+    it('toggle full description should toggle highlight task', function () {
+        var task = {$highlight: false, notes: 'test'};
+        directive.scope.showFullDescription(task, {highlightTaskOnClick: true}, {
+            stopPropagation: angular.noop
+        });
+        expect(kanbanMock.highlightTask).toHaveBeenCalled();
+    });
+
+    it('should remove task', function () {
+        var task = {$highlight: false, notes: 'test', remove: jasmine.createSpy('remove')};
+        directive.scope.deleteTask({
+            stopPropagation: angular.noop
+        }, task);
+        directive.scope.$apply();
+        //expect(task.remove).toHaveBeenCalled();
+    });
 });
