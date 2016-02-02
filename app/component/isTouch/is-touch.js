c0589a350ce80e11c559afccd467f27c2e67441b
Stanislau Tsiuryn <stanislau@cog1.com>
2016-02-03 00:18:47 +0300

    DE8601: Drag and drop issues

--- app/component/isTouch/is-touch.js
+++ app/component/isTouch/is-touch.js
@@ -1,6 +1,6 @@
 angular.module('component.is-touch', []).provider('isTouch', function () {
     this.$get = function ($window) {
-        return (/Mobile|iP(hone|od|ad)|Android|BlackBerryt|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/).test($window.navigator.userAgent);
+        return (/Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile/).test($window.navigator.userAgent);
     };
 }).factory('isNotTouch', function(isTouch){
     return !isTouch;
