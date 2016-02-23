a8750871dab3544b0e6d4d5f7091fa5a89bf03df
Vladimir Shein <vladimir.sonnenhaft@gmail.com>
2016-02-23 19:23:05 +0100

    removed kanban-version directive - reused one from cog1 rl-integrations repo

--- app/demo-code/kanban-demo.js
+++ app/demo-code/kanban-demo.js
@@ -1,9 +1,7 @@
 angular.module('kanban-demo', [
+    'cog1.package-version' ,
     'demo-code.events-debugger',
-    'demo-code.kanban-version' ,
     'demo-code.layout-switcher',
     'demo-code.groups-debug',
     'kanban'
-]).config(function(kanbanVersionValueProvider, kanbanVersion){
-    kanbanVersionValueProvider.set(kanbanVersion);
-});
+]);
