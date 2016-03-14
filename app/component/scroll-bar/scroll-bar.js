3c4aaa077ee2c53f8d78b679f6d04eabf6638fa5
Vladimir Shein <vlad@cog1.com>
2016-03-14 16:00:29 +0100

    fixed case of link params

--- app/component/scroll-bar/scroll-bar.js
+++ app/component/scroll-bar/scroll-bar.js
@@ -1,26 +1,24 @@
-angular
-    .module('component.scroll-bar')
-    .directive('scrollbar', function () {
-        return {
-            templateUrl: 'app/component/scroll-bar/scroll-bar.html',
-            scope: {
-                columns: '='
-            },
-            require: '^combineHorizontalScrolls',
-            link: function (scope, element, attrs, ctrl) {
-                ctrl.scrollBarElement = element[0];
+angular.module('component.scroll-bar').directive('scrollbar', function () {
+    return {
+        templateUrl: 'app/component/scroll-bar/scroll-bar.html',
+        scope: {
+            columns: '='
+        },
+        require: '^combineHorizontalScrolls',
+        link: function ( $scope, $element, $attrs, combineHorizontalScrolls ) {
+            combineHorizontalScrolls.scrollBarElement = $element[ 0 ];
 
-                scope.$on('$destroy', function () {
-                    ctrl.scrollBarElement = null;
-                });
-            }
-        };
-    }).directive('columnsWidth', function () {
-        return {
-            restrict: 'A',
-            require: '^kanban',
-            link: function (scope, element, attrs, kanban) {
-                kanban.registerElement(element, scope);
-            }
-        };
-    });
+            $scope.$on('$destroy', function () {
+                combineHorizontalScrolls.scrollBarElement = null;
+            });
+        }
+    };
+}).directive('columnsWidth', function () {
+    return {
+        restrict: 'A',
+        require: '^kanban',
+        link: function ( $scope, $element, ignored, kanban ) {
+            kanban.registerElement($element, $scope);
+        }
+    };
+});
